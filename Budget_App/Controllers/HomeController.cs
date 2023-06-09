f1b234f557058ce3a4a6ec2a7a4024f1553ed10d
Dharanidharan <dharanidharansivasamy@gmail.com>
2023-06-09 23:24:55 +0530

    Daywise split added

--- Budget_App/Controllers/HomeController.cs
+++ Budget_App/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Budget_App.Models;
+﻿using Budget_App.Data;
+using Budget_App.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,10 +8,12 @@ namespace Budget_App.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDBContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,ApplicationDBContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -20,7 +23,19 @@ namespace Budget_App.Controllers
 
         public IActionResult Privacy()
         {
-            return View();
+            var splitList = _context.Expenses.GroupBy(c => c.SpentDate)
+                .Select(x => new
+                {
+                    Date=x.Key,
+                    Spends = x.Select(y => new
+                    {
+                        ExpenseName=y.ExpenseName,
+                        ExpenseDescription=y.ExpenseDescription,
+                        ExpenseAmmount=y.ExpenseAmt
+                    })
+                })
+                .ToList();
+            return View(splitList);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
